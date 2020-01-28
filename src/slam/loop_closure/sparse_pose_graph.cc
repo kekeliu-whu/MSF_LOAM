bfaabfdec9a5cc5de8c90977e2952dae620e1578
kekeliu <rickyrickc132@gmail.com>
2020-01-28 17:38:49 +0800

    feat(graph): add sparse pose graph optimization feature

---
+++ src/slam/loop_closure/sparse_pose_graph.cc
@@ -0,0 +1,10 @@
+//
+// Created by whu on 1/16/20.
+//
+
+#include "slam/loop_closure/sparse_pose_graph.h"
+#include "slam/loop_closure/pose_graph_factor.h"
+
+SparsePoseGraph::SparsePoseGraph() {}
+
+SparsePoseGraph::~SparsePoseGraph() {}
