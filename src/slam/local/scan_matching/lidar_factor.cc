3a1c63abd64adc6ce263a7910c12bcf7508d6769
kekeliu <rickyrickc132@gmail.com>
2021-11-15 22:37:29 +0800

    support LidarEdgeFactorDeskewSE3

--- src/slam/local/scan_matching/lidar_factor.cc
+++ src/slam/local/scan_matching/lidar_factor.cc
@@ -5,7 +5,7 @@
 #include "slam/local/scan_matching/lidar_factor.h"
 
 bool LidarEdgeFactorSE3::Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
-  Eigen::Map<Eigen::Matrix<double, 3, 1>> residual(residuals);
+  Eigen::Map<Eigen::Matrix<double, kResidualNums, 1>> residual(residuals);
 
   Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
   Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
@@ -13,10 +13,10 @@ bool LidarEdgeFactorSE3::Evaluate(double const *const *parameters, double *resid
 
   if (jacobians) {
     if (jacobians[0]) {
-      Eigen::Map<Eigen::Matrix<double, 3, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
       jacobian_pose_i.setZero();
-      jacobian_pose_i.block<3, 3>(0, 0) = Utility::skewSymmetric(last_line_N_);
-      jacobian_pose_i.block<3, 3>(0, 3) = -Utility::skewSymmetric(last_line_N_) * (Qi.toRotationMatrix() * Utility::skewSymmetric(curr_point_));
+      jacobian_pose_i.block<kResidualNums, 3>(0, 0) = Utility::skewSymmetric(last_line_N_);
+      jacobian_pose_i.block<kResidualNums, 3>(0, 3) = -Utility::skewSymmetric(last_line_N_) * (Qi.toRotationMatrix() * Utility::skewSymmetric(curr_point_));
     }
   }
 
@@ -24,7 +24,7 @@ bool LidarEdgeFactorSE3::Evaluate(double const *const *parameters, double *resid
 }
 
 bool LidarPlaneFactorSE3::Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
-  Eigen::Map<Eigen::Matrix<double, 1, 1>> residual(residuals);
+  Eigen::Map<Eigen::Matrix<double, kResidualNums, 1>> residual(residuals);
 
   Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
   Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
@@ -33,10 +33,64 @@ bool LidarPlaneFactorSE3::Evaluate(double const *const *parameters, double *resi
 
   if (jacobians) {
     if (jacobians[0]) {
-      Eigen::Map<Eigen::Matrix<double, 1, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
       jacobian_pose_i.setZero();
-      jacobian_pose_i.block<1, 3>(0, 0) = last_plane_N_;
-      jacobian_pose_i.block<1, 3>(0, 3) = -last_plane_N_.transpose() * (Qi.toRotationMatrix() * Utility::skewSymmetric(curr_point_));
+      jacobian_pose_i.block<kResidualNums, 3>(0, 0) = last_plane_N_;
+      jacobian_pose_i.block<kResidualNums, 3>(0, 3) = -last_plane_N_.transpose() * (Qi.toRotationMatrix() * Utility::skewSymmetric(curr_point_));
+    }
+  }
+
+  return true;
+}
+
+bool LidarEdgeFactorDeskewSE3::Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
+  Eigen::Map<Eigen::Matrix<double, kResidualNums, 1>> residual(residuals);
+
+  Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
+  Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
+  Eigen::Vector3d Vi(parameters[1][0], parameters[1][1], parameters[1][2]);
+
+  residual = last_line_N_.cross(Qi * (delta_q_ * curr_point_ + delta_p_) + Vi * dt_ - 0.5 * G_ * dt_ * dt_ + Pi - last_line_C_);
+
+  if (jacobians) {
+    if (jacobians[0]) {
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
+      jacobian_pose_i.setZero();
+      jacobian_pose_i.block<kResidualNums, 3>(0, 0) = Utility::skewSymmetric(last_line_N_);
+      jacobian_pose_i.block<kResidualNums, 3>(0, 3) = -Utility::skewSymmetric(last_line_N_) *
+                                                      (Qi.toRotationMatrix() * Utility::skewSymmetric(delta_q_ * curr_point_ + delta_p_));
+    }
+
+    if (jacobians[1]) {
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 3, Eigen::RowMajor>> jacobian_bias_i(jacobians[0]);
+      jacobian_bias_i = Utility::skewSymmetric(last_line_N_) * dt_;
+    }
+  }
+
+  return true;
+}
+
+bool LidarPlaneFactorDeskewSE3::Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
+  Eigen::Map<Eigen::Matrix<double, kResidualNums, 1>> residual(residuals);
+
+  Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
+  Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
+  Eigen::Vector3d Vi(parameters[1][0], parameters[1][1], parameters[1][2]);
+
+  residual(0, 0) = last_plane_N_.dot(Qi * (delta_q_ * curr_point_ + delta_p_) + Vi * dt_ - 0.5 * G_ * dt_ * dt_ + Pi - last_plane_C_);
+
+  if (jacobians) {
+    if (jacobians[0]) {
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
+      jacobian_pose_i.setZero();
+      jacobian_pose_i.block<kResidualNums, 3>(0, 0) = last_plane_N_.transpose();
+      jacobian_pose_i.block<kResidualNums, 3>(0, 3) = -last_plane_N_.transpose() *
+                                                      (Qi.toRotationMatrix() * Utility::skewSymmetric(delta_q_ * curr_point_ + delta_p_));
+    }
+
+    if (jacobians[1]) {
+      Eigen::Map<Eigen::Matrix<double, kResidualNums, 3, Eigen::RowMajor>> jacobian_bias_i(jacobians[0]);
+      jacobian_bias_i = last_plane_N_.transpose() * dt_;
     }
   }
 
--- src/slam/local/scan_matching/lidar_factor.h
+++ src/slam/local/scan_matching/lidar_factor.h
@@ -4,7 +4,6 @@
 #pragma once
 
 #include <ceres/ceres.h>
-#include <Eigen/Core>
 
 #include "slam/imu_fusion/utility.h"
 
@@ -23,6 +22,40 @@ struct LidarEdgeFactorSE3 : ceres::SizedCostFunction<3, 7> {
   const Eigen::Vector3d curr_point_;
   const Eigen::Vector3d last_line_C_;
   const Eigen::Vector3d last_line_N_;
+
+  static constexpr int kResidualNums = 3;
+};
+
+struct LidarEdgeFactorDeskewSE3 : ceres::SizedCostFunction<3, 7> {
+ public:
+  LidarEdgeFactorDeskewSE3(const Eigen::Vector3d &curr_point,
+                           const Eigen::Vector3d &last_line_C,
+                           const Eigen::Vector3d &last_line_N,
+                           const Eigen::Vector3d &delta_p,
+                           const Eigen::Quaterniond &delta_q,
+                           const double &dt,
+                           const Eigen::Vector3d &G)
+      : curr_point_(curr_point),
+        last_line_C_(last_line_C),
+        last_line_N_(last_line_N),
+        delta_p_(delta_p),
+        delta_q_(delta_q),
+        dt_(dt),
+        G_(G) {}
+
+  bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const;
+
+ private:
+  const Eigen::Vector3d curr_point_;
+  const Eigen::Vector3d last_line_C_;
+  const Eigen::Vector3d last_line_N_;
+
+  const Eigen::Vector3d delta_p_;
+  const Eigen::Quaterniond delta_q_;
+  const double dt_;
+  const Eigen::Vector3d G_;
+
+  static constexpr int kResidualNums = 3;
 };
 
 struct LidarPlaneFactorSE3 : ceres::SizedCostFunction<1, 7> {
@@ -50,4 +83,38 @@ struct LidarPlaneFactorSE3 : ceres::SizedCostFunction<1, 7> {
   const Eigen::Vector3d curr_point_;
   const Eigen::Vector3d last_plane_C_;
   const Eigen::Vector3d last_plane_N_;
+
+  static constexpr int kResidualNums = 1;
+};
+
+struct LidarPlaneFactorDeskewSE3 : ceres::SizedCostFunction<1, 7> {
+ public:
+  LidarPlaneFactorDeskewSE3(const Eigen::Vector3d &curr_point,
+                            const Eigen::Vector3d &last_plane_C,
+                            const Eigen::Vector3d &last_plane_N,
+                            const Eigen::Vector3d &delta_p,
+                            const Eigen::Quaterniond &delta_q,
+                            const double &dt,
+                            const Eigen::Vector3d &G)
+      : curr_point_(curr_point),
+        last_plane_C_(last_plane_C),
+        last_plane_N_(last_plane_N),
+        delta_p_(delta_p),
+        delta_q_(delta_q),
+        dt_(dt),
+        G_(G) {}
+
+  virtual bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const;
+
+ private:
+  const Eigen::Vector3d curr_point_;
+  const Eigen::Vector3d last_plane_C_;
+  const Eigen::Vector3d last_plane_N_;
+
+  const Eigen::Vector3d delta_p_;
+  const Eigen::Quaterniond delta_q_;
+  const double dt_;
+  const Eigen::Vector3d G_;
+
+  static constexpr int kResidualNums = 1;
 };
